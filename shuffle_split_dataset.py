83b6d15a0f0fa52c2dcc4903c830deab36fb2fc9
Dolapo Olatoye <dolaposalim@gmail.com>
2025-06-04 00:10:41 +0200

    New code line

--- shuffle_split_dataset.py
+++ shuffle_split_dataset.py
@@ -4,9 +4,20 @@ import pandas as pd
 import tarfile
 import urllib.request
 
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarbell:
+            housing_tarbell.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing.csv"))
+
 def shuffle_and_split_data (data, test_ratio):
   np.random.seed(42)
-  shuffled_indices = np.random.permutation(len(data)
+  shuffled_indices = np.random.permutation(len(data))
   test_set_size = int(len(data) * test_ratio)
   test_indices = shuffled_indices[:test_set_size]
   train_indices = shuffled_indices[test_set_size:]
